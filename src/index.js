76f3e2195e47451e477e1207c5860273edd2923e
leahraquel <leah170072matthews@gmail.com>
2025-03-13 16:08:57 +0000

    minor changes to context

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ function generateRecipe(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "o4b92af1dd47taab0064a1fe60a4583f";
   let context =
-    "You are a culinary expert in dishes made from Spain. Your mission is to generate a Spanish recipe in basic HTML. Don't include acknowledgement of HTML. Make sure to follow the user instructions below.";
+    "You are a culinary expert in dishes made from Spain. Your mission is to generate a Spanish recipe in basic HTML. Don't include acknowledgement of HTML. Make sure to follow the user instructions below. Make the recipe simple and easy to read.";
   let prompt = `User instructions: Generate a recipe for ${instructionsInput.value} in English`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
