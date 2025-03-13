330d70c84ce0b5ccb0097a686cae0a0397727c48
leahraquel <leah170072matthews@gmail.com>
2025-03-13 15:24:02 +0000

    added basic form handling + typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "recipe",
+    autoStart: true,
+    delay: 8,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
