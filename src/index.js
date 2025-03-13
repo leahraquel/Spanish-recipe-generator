ee9fb99c5f21b5593145f901d4b746f074d2093d
leahraquel <leah170072matthews@gmail.com>
2025-03-13 16:05:56 +0000

    added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function generateRecipe(event) {
-  event.preventDefault();
-
+function displayRecipe(response) {
+  console.log("recipe generator");
   new Typewriter("#recipe", {
-    strings: "recipe",
+    strings: response.data.answer,
     autoStart: true,
     delay: 8,
     cursor: "",
   });
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "o4b92af1dd47taab0064a1fe60a4583f";
+  let context =
+    "You are a culinary expert in dishes made from Spain. Your mission is to generate a Spanish recipe in basic HTML. Don't include acknowledgement of HTML. Make sure to follow the user instructions below.";
+  let prompt = `User instructions: Generate a recipe for ${instructionsInput.value} in English`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating recipe");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
